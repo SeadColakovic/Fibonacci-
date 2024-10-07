f1c79d18e9be5d6761e8d31b8d5303440b3a645f
SeadColakovic <flegarsead263@gmail.com>
2024-10-07 13:57:15 +0200

    Add files via upload

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, rezultat;
+            Console.WriteLine("*** Fibonaccijev niz ***");
+            Console.WriteLine();
+            Console.WriteLine("Unesi broj n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            rezultat = Fibonacci(n);
+
+            Console.WriteLine("{0} - ti član Fibonaccijevog niza je : {1}", n, rezultat);
+            Console.ReadKey();
+        }
+
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int rezultat = 0;
+                
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                Console.WriteLine("1. - 1");
+                for (int i = 2; i <= n; i++)
+                {
+                    rezultat = a + b;
+                    a = b;
+                    b = rezultat;
+                    Console.WriteLine(i + ". - " + rezultat);
+                }
+                return rezultat;
+            }
+        }
+    }
+}
+
